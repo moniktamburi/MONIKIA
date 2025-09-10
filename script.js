d650a4693fa30c80fd8924b9ceca885f5ec756c0
moniktamburi <monik.araujo@escola.pr.gov.br>
2025-09-10 20:58:13 -0300

    ok

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaprincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixs-perguntas");
+const caixaAlternativass=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa=resultado");
+cont textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:"assim que saiu do colégio você se depara com uma nova tecnologia, em chat que consegue responder todas as dúvidas que uma pessoa ter, ele também gera imagens e a áudios. qual é o seu prieiro pensamento?",
+        alternativas:[
+            {
+            texto:"isso é assustador",
+            afirmação:"No início, eu ficaria com medo d que essa tecnologia pode faer."
+
+            },
+            {
+                
+            }
+        ]
+    }
+]
