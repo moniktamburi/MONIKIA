fcb1229cf630525f0fcbd13c6272ed5fa5b1ef53
moniktamburi <monik.araujo@escola.pr.gov.br>
2025-09-25 20:55:32 -0300

    ok

--- script.js
+++ script.js
@@ -89,5 +89,28 @@ function mostraPergunta(){
 }
   
 function mostraAlternativas(){
-    for(cont alternativa of perguntaAtual.alternativas)
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
+    
+
 }
